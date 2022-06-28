033b04a3802a2162020626ef3ea37f1f04c73172
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-06-28 15:08:01 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if chr(c) == 'e' or chr(c) == q:
+    if chr(c) == 'e' or chr(c) == 'q':
         continue
         print("{:c}".format(c), end="")
