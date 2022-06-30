55d3cf1779ecb226e770b1a868f481c40e86a6c5
benjamineruvieru <benjamineruvieru@gmail.com>
2022-06-30 15:11:32 +0000

    ff

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 0:
+        print('0 arguments.')
+    else:
+        print('{} arguments :'.format(len(sys.argv)
+        for i in range(0, len(sys.argv)):
+            print(len(sys.argv))
