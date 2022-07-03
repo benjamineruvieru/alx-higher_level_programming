60ce9b53c28925c340dadfb17de0456fc399845b
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-07-03 20:39:56 +0100

    Create 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    result = ()
+    if len(sentence) > 0:
+        lenght = len(sentence)
+        first_cha = sentence[0]
+        result = (lenght, first_cha)
+    else:
+        result = 'None'
+    return result
