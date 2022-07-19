0db9177f5260be6e559987b13db2013eb7b6c7be
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-07-19 20:25:30 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" 
-This is Square module doc
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes
 """
 
-class Square:
-    """ Represents a Square
-    Private instance attribute: size
-    Instantiation with optional size.
-    """
 
-    __init__(self, size=0):
-        """Initializes the data"""
-        if not isinstance(size, int):
+class Square:
+    """Square implementation
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
