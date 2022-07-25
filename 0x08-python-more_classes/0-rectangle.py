82ba24a1990ab0976d7c12c70f1086ed06a4f28f
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-07-25 13:27:38 +0100

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,8 @@
-""" A Rectangle module """"
+#!/usr/bin/python3
+"""Module 0-rectangle
+Defines an empty Rectangle class.
+"""
 
 class Rectangle:
-""" A class the defines a Rectangle"""    
+    """ A class the defines a Rectangle"""    
     pass
