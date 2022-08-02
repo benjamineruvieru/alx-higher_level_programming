9d2072cbf4790eb5eb93df6096fa0d2ef4765c86
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-08-02 20:06:52 +0100

    complete

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,7 +7,7 @@ import json
 def to_json_string(my_obj):
     """A function that returns the JSON representation of an object
     Args:
-            -	my_obj
+                    -	my_obj
 
     Returns:  JSON representation of an object	
     """
