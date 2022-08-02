5e27c301896ac7bdf67935f299d2cef45f066dd0
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-08-02 14:17:35 +0100

    task 2

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 2."""
+
+
+def write_file(filename="", text=""):
+    """ a function that writes a string to a text file (UTF8)
+    and returns the number of characters written."""
+    with open(filename, "w") as f:
+        f.write(text)
