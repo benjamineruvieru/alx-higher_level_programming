fe766ea375e1e8fc22f9920cad7215e558865c49
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-08-02 19:57:43 +0100

    tasks 4

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Module 3 yh."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ a function that appends a string to a text file (UTF8)
     and returns the number of characters written."""
     with open(filename, "a") as f:
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 4."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """A function that returns the JSON representation of an object
+    Args:
+            -	my_obj
+
+    Returns:  JSON representation of an object	
+    """
+    return json.dumps(my_obj)
