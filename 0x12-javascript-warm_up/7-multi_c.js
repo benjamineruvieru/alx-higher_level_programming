f1b521ed2af5811712ea2e29051e705772a00885
ben <benjamineruvieru@gmail.com>
2022-10-03 18:19:16 +0100

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const convertedNum = parseInt(process.argv[2]);
+if (isNaN(convertedNum)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < convertedNum; i++) {
+    console.log('C is fun');
+  }
+}
