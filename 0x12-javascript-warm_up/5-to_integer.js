a142b3892ac60a86730cd8da9e0241b0c03d6ea0
ben <benjamineruvieru@gmail.com>
2022-10-03 14:03:59 +0100

    yh

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const convertedNum = parseInt(process.argv[2]);
+if (isNaN(convertedNum)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', convertedNum);
+}
