a3168d050bacffdde39249f1941f5f43ea183067
ben <benjamineruvieru@gmail.com>
2022-10-03 18:46:33 +0100

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const convertedNum = parseInt(process.argv[2]);
+let factorial = 1;
+if (isNaN(convertedNum)) {
+  console.log(1);
+} else {
+  fac(1);
+}
+function fac (num) {
+  factorial = num * factorial;
+  const nextnum = num + 1;
+  if (nextnum <= convertedNum) {
+    fac(nextnum);
+  } else {
+    console.log(factorial);
+  }
+}
