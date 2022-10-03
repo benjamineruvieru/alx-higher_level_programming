b9e9274643fe9f2e917f61791a232d31320f6c23
ben <benjamineruvieru@gmail.com>
2022-10-03 13:43:36 +0100

    f

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (argv.length > 2) {
+if (argv.length = 3) {
   console.log('Argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
