63dc1791468e3a1d1228abebe32988f6c608e514
ben <benjamineruvieru@gmail.com>
2022-10-03 18:31:03 +0100

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const convertedNum = parseInt(process.argv[2]);
+if (isNaN(convertedNum)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < convertedNum; i++) {
+    console.log('X'.repeat(convertedNum));
+  }
+}
