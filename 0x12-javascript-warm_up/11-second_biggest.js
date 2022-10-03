8d0b15124be85ff91f77a6044a907c5d67bc0a91
ben <benjamineruvieru@gmail.com>
2022-10-03 19:21:27 +0100

    y

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let list = process.argv.slice(2);
+const list = process.argv.slice(2);
 if (list.length === 1) {
   console.log(0);
 } else if (list.length === 0) {
@@ -7,5 +7,5 @@ if (list.length === 1) {
 } else {
   const largest = list.reduce((a, b) => Math.max(parseInt(a), parseInt(b)));
   list.splice(list.indexOf(largest.toString()), 1);
-  console.log(list.reduce((a, b) => Math.max(parseInt(a), parseInt(b))));
+  console.log(parseInt(list.reduce((a, b) => Math.max(parseInt(a), parseInt(b)))));
 }
