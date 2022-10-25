74392f87cba69a1cc3e8498938aa5924138731ad
ben <benjamineruvieru@gmail.com>
2022-10-25 21:53:24 +0100

    yh

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,13 @@
-#!/usr/bin/python3 
-"""Module for fetching."""
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
 
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
